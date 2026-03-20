722ce67bc309271a0fdf00cba39bdb1d63a5f935
keremozt <keremoztopuzzz@gmail.com>
2026-03-20 21:29:54 +0300

    feat: implement basic flight controls including speed adjustment, movement, and rotation in `FlightController.cs`.

--- Assets/Scripts/FlightController.cs
+++ Assets/Scripts/FlightController.cs
@@ -22,6 +22,46 @@ public class FlightController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position -= transform.forward * speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space)) {
+            if (speed < maxSpeed) {
+                speed += acceleration * Time.deltaTime;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            if (speed > 0) {
+                speed -= deceleration * Time.deltaTime;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A) && transform.position.y > 20f) {
+            transform.Rotate(Vector3.back*Time.deltaTime*turnSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.D) && transform.position.y > 20f) {
+            transform.Rotate(Vector3.forward*Time.deltaTime*turnSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S) && speed >= 100f) {
+            transform.Rotate(Vector3.right*Time.deltaTime*turnSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.W) && speed >= 100f) {
+            transform.Rotate(Vector3.left*Time.deltaTime*turnSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.Q)) {
+            transform.Rotate(Vector3.up*Time.deltaTime*turnSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.E)) {
+            transform.Rotate(Vector3.down*Time.deltaTime*turnSpeed);
+        }
+
+        if (transform.position.y < 2f) {
+            transform.position = new Vector3(transform.position.x, 2f, transform.position.z);
+        }
     }
 }
