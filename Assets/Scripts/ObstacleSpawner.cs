fd8826aa165c898d678702b2a7a53f200d5bbae0
keremozt <keremoztopuzzz@gmail.com>
2026-03-22 00:48:11 +0300

    feat: Add obstacle prefab, plane transform, and timing variables, and implement a plane height check in ObstacleSpawner.

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
+
+    public GameObject SkyScraperPrefab;
+    public Transform PlaneTransform;
+
+    private float time = 0f;
+    private float spawnTime = 2f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +18,8 @@ public class ObstacleSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (PlaneTransform == null || PlaneTransform.position.y < 20f) {
+            return;
+        }
     }
 }
