942432b49fc2af53514085144377f16c164cfa57
keremozt <keremoztopuzzz@gmail.com>
2026-03-22 01:20:44 +0300

    feat: Implement timed skyscraper spawning logic and adjust the spawn interval.

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -2,12 +2,11 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-
     public GameObject SkyScraperPrefab;
     public Transform PlaneTransform;
 
     private float time = 0f;
-    private float spawnTime = 2f;
+    private float spawnTime = 4f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,5 +20,21 @@ public class ObstacleSpawner : MonoBehaviour
         if (PlaneTransform == null || PlaneTransform.position.y < 20f) {
             return;
         }
+
+        time += Time.deltaTime;
+        
+        if (time >= spawnTime) {
+            time = 0f;
+            
+            Vector3 spawnPosition = PlaneTransform.position - (PlaneTransform.forward * 5000f);
+            
+            spawnPosition.y = 0f;
+            spawnPosition.x += Random.Range(-50f, 50f);
+
+            GameObject newTower = Instantiate(SkyScraperPrefab, spawnPosition, Quaternion.identity);
+            Destroy(newTower, 10f);
+        }
+
+        
     }
 }
