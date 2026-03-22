b355e01f55392ea986f2b02f8f3e70a447310188
keremozt <keremoztopuzzz@gmail.com>
2026-03-22 14:27:08 +0300

    feat: Add UI elements for game state management and implement start text dismissal on 'M' key press.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public Text startText;
+    public Text gameOverText;
+    public GameObject tryAgainButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +16,8 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (startText.gameObject.activeSelf && Input.GetKeyDown(KeyCode.M)) {
+            startText.gameObject.SetActive(false);
+        }
     }
 }
